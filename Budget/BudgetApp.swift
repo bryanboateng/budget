7ba640beae068de62563b3e867d7635a4affc1b3
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2021-03-20 21:45:52 +0100

    Switch to SwiftUI

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Bryan Oppong-Boateng on 20.03.21.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
