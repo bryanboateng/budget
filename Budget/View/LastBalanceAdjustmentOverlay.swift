216c236c9b98ea6a44d39e8c465a0413a7492ea5
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2022-02-14 10:01:54 +0100

    Fix Last Balance Adjusment Overlay Bug #11

--- Budget/View/LastBalanceAdjustmentOverlay.swift
+++ Budget/View/LastBalanceAdjustmentOverlay.swift
@@ -4,30 +4,31 @@ struct LastBalanceAdjustmentOverlay: ViewModifier {
     @Binding var budget: Budget?
     
     func body(content: Content) -> some View {
-        if let lastBalanceAdjustment = budget?.lastBalanceAdjustment {
-            content
-                .overlay(
-                    Text("\(lastBalanceAdjustment.decimalValue.formatted(.eur().sign(strategy: .always())))")
-                        .font(.system(size: 80, weight: .semibold))
-                        .lineLimit(1)
-                        .minimumScaleFactor(0.1)
-                        .padding(8)
-                        .frame(maxWidth: .infinity)
-                        .background(
-                            RoundedRectangle(cornerRadius: 8)
-                                .foregroundStyle(Material.thin)
-                        )
-                        .padding(.horizontal, 6)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .background(Color.red.opacity(0.001))
-                        .onTapGesture {
-                            budget = nil
-                        }
-                        .colorScheme(.dark)
-                )
-        } else {
-            content
-        }
+        content
+            .overlay(
+                Group {
+                    if let lastBalanceAdjustment = budget?.lastBalanceAdjustment {
+                        Text(lastBalanceAdjustment.decimalValue.formatted(.eur().sign(strategy: .always())))
+                            .font(.system(size: 80, weight: .semibold))
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.1)
+                            .padding(8)
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .foregroundStyle(Material.thin)
+                            )
+                            .padding(.horizontal, 6)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(Color.red.opacity(0.001))
+                            .onTapGesture {
+                                budget = nil
+                            }
+                            .colorScheme(.dark)
+                    }
+                }
+                    .opacity(budget?.lastBalanceAdjustment == nil ? 0 : 1)
+            )
     }
 }
 
