0067e3d14626e554ce60dcdb28ef6c3e3e8b4fe1
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2022-03-25 16:55:48 +0100

    Add empty states

--- Budget/View/BudgetList.swift
+++ Budget/View/BudgetList.swift
@@ -19,82 +19,98 @@ struct BudgetList: View {
     }
     
     var body: some View {
-        List {
-            Text("Gesamter Stand")
-                .font(.headline)
-                .badge(totalBalance.formatted(.eur()))
-            ForEach(model.categories) { category in
-                Section(
-                    header:
-                        HStack {
-                            Text(category.name)
-                            Spacer()
-                            Button {
-                                categoryBeingExtended = category
-                            } label: {
-                                Label("Neues Budget", systemImage: "plus")
-                                    .labelStyle(.iconOnly)
+        Group {
+            if !model.categories.isEmpty {
+                List {
+                    Text("Gesamter Stand")
+                        .font(.headline)
+                        .badge(totalBalance.formatted(.eur()))
+                    ForEach(model.categories) { category in
+                        Section(
+                            header:
+                                HStack {
+                                    Text(category.name)
+                                    Spacer()
+                                    Button {
+                                        categoryBeingExtended = category
+                                    } label: {
+                                        Label("Neues Budget", systemImage: "plus")
+                                            .labelStyle(.iconOnly)
+                                    }
+                                }
+                        ) {
+                            Group {
+                                if !category.budgets.isEmpty {
+                                    ForEach(
+                                        category.budgets.sorted { lhs, rhs in
+                                            if lhs.balance == rhs.balance {
+                                                return lhs.name < rhs.name
+                                            }
+                                            return lhs.balance > rhs.balance
+                                        }
+                                    ) { budget in
+                                        Menu {
+                                            Button {
+                                                budgetAdjustingBalance = budget
+                                            } label: {
+                                                Label("Saldo anpassen", systemImage: "arrow.left.arrow.right")
+                                            }
+                                            
+                                            Button {
+                                                budgetWhosHistoryIsShown = budget
+                                            } label: {
+                                                Label("Historie", systemImage: "clock")
+                                            }
+                                            
+                                            Button {
+                                                budgetBeingEdited = budget
+                                            } label: {
+                                                Label("Bearbeiten", systemImage: "pencil")
+                                            }
+                                            
+                                            Button(role: .destructive) {
+                                                budgetBeingDeleted = budget
+                                            } label: {
+                                                Label("Löschen", systemImage: "trash")
+                                            }
+                                        } label: {
+                                            HStack {
+                                                BudgetRow(budget: budget, color: category.color)
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    Text("In dieser Kategorie gibt es keine Budgets.")
+                                        .foregroundStyle(.secondary)
+                                        .multilineTextAlignment(.center)
+                                        .frame(maxWidth: .infinity)
+                                }
                             }
                         }
-                ) {
-                    ForEach(
-                        category.budgets.sorted { lhs, rhs in
-                            if lhs.balance == rhs.balance {
-                                return lhs.name < rhs.name
-                            }
-                            return lhs.balance > rhs.balance
+                        .headerProminence(.increased)
+                        .sheet(item: $budgetBeingEdited) { budget in
+                            BudgetEditor(budget: budget, category: category)
                         }
-                    ) { budget in
-                        Menu {
-                            Button {
-                                budgetAdjustingBalance = budget
-                            } label: {
-                                Label("Saldo anpassen", systemImage: "arrow.left.arrow.right")
-                            }
-                            
-                            Button {
-                                budgetWhosHistoryIsShown = budget
-                            } label: {
-                                Label("Historie", systemImage: "clock")
-                            }
-                            
-                            Button {
-                                budgetBeingEdited = budget
-                            } label: {
-                                Label("Bearbeiten", systemImage: "pencil")
-                            }
-                            
-                            Button(role: .destructive) {
-                                budgetBeingDeleted = budget
-                            } label: {
-                                Label("Löschen", systemImage: "trash")
-                            }
-                        } label: {
-                            HStack {
-                                BudgetRow(budget: budget, color: category.color)
-                            }
+                        .sheet(item: $budgetAdjustingBalance) { budget in
+                            BalanceAdjuster(budget: budget, category: category)
+                        }
+                        .actionSheet(item: $budgetBeingDeleted) { budget in
+                            ActionSheet(
+                                title: Text("\(budget.name) löschen"),
+                                message: Text("Soll das Budget \(budget.name) wirklich gelöscht werden?"),
+                                buttons: [
+                                    .destructive(Text("Budget löschen")) {
+                                        model.delete(budget, inCategory: category)
+                                    },
+                                    .cancel()
+                                ]
+                            )
                         }
                     }
                 }
-                .headerProminence(.increased)
-                .sheet(item: $budgetBeingEdited) { budget in
-                    BudgetEditor(budget: budget, category: category)
-                }
-                .sheet(item: $budgetAdjustingBalance) { budget in
-                    BalanceAdjuster(budget: budget, category: category)
-                }
-                .actionSheet(item: $budgetBeingDeleted) { budget in
-                    ActionSheet(
-                        title: Text("\(budget.name) löschen"),
-                        message: Text("Soll das Budget \(budget.name) wirklich gelöscht werden?"),
-                        buttons: [
-                            .destructive(Text("Budget löschen")) {
-                                model.delete(budget, inCategory: category)
-                            },
-                            .cancel()
-                        ]
-                    )
-                }
+            } else {
+                Text("Keine Kategorien.")
+                    .foregroundStyle(.secondary)
             }
         }
         .navigationTitle("Budgets")
